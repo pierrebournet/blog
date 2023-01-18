cbe2062550e8ab860f9f9d2de7f239d2d53572b6
DE URTASUN Sokunthy (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7908391c@commun.ad.sncf.fr>
2023-01-18 09:41:47 +0100

    ajouts articles et commaentary services

--- src/services/usersServices.js
+++ src/services/usersServices.js
@@ -1,6 +1,6 @@
 class UsersServices {
     async getUserByName(name) { 
-        const data = await client.query('SELECT * FROM users WHERE username = $1', [name]);
+        const data = await client.query('SELECT * FROM users WHERE name = $1', [name]);
 console.log(data);
         if (data.rowCount > 0) {
             return data.rows[0]
